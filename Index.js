28a7b7feddfdfd3534bd5b7140dfab2fb5c86600
wvjr <williamvazdireito@gmail.com>
2023-12-29 08:56:58 -0300

    Projeto concluído

--- Index.js
+++ Index.js
@@ -1,2 +1,17 @@
-testando novamente
-testeando de novo
+numVitorias (" ");
+
+function numVitorias (x){
+
+let userName = ("Mario");
+let victory = 15;
+derrota = 10;
+result = (victory - derrota);
+
+if (result < 10)
+   {console.log("O Herói " + userName + " tem saldo de " + victory + " pontos e está no nível de Ferro!")}
+   
+else if(result >= 10)
+       {console.log("O Herói " + userName +  " tem saldo de " + victory + " pontos e está no nível de Prata!")}
+ 
+ 
+};
